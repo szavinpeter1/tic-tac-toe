35699da3a9d81b61244825b106b2554794ecc91e
szavinpeter1 <szavinpeter1@gmail.com>
2022-04-24 19:10:32 +0200

    update table on click

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+let table = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+let allTds = document.querySelectorAll("td")
+for(let td of allTds) {
+  //  console.log(td.id)
+  td.addEventListener("click",function() {
+      console.log(td.id)
+    let row = parseInt(td.id[0])
+    let column = parseInt(td.id[1])
+    table[row][column] = 1
+    console.log(table)
+  })
+}
